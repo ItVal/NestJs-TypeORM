70a26ce5811527acc5913d1073a91e53d2023abb
Valentin Nasibu <irnasvalentin@gmail.com>
2025-05-14 00:52:51 +0200

    feat:define root in users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
     @Get()
         findAll(){
             return 'This action returns all users';
+        }
+    @Get(':id')
+        findOne(@Param() id){
+            return id;
+        }
+    
+    @Post()
+    @HttpCode(202)
+        create(@Body() body){
+            return body;   
         }
 
     
