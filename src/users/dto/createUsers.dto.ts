62fd7308a82c85ee7bc4b810aa2d61e5766efb00
Valentin Nasibu <irnasvalentin@gmail.com>
2025-05-14 16:46:26 +0200

    feat:group dto validation

--- src/users/dto/createUsers.dto.ts
+++ src/users/dto/createUsers.dto.ts
@@ -1,18 +1,19 @@
-import { IsString, IsNumber, Length, IsPositive } from 'class-validator';
+import { IsString, Length, IsPositive, IsInt } from 'class-validator';
 export class createUsersDto {
     @IsString()
-    @Length(3, 20, { message: 'Name must be between 3 and 20 characters' })
+    @Length(4, 20, { message: 'Name must be between 4 and 20 characters for creation', groups: ['create'] })
+    @Length(3, 20, { message: 'Name must be between 3 and 20 characters for update', groups: ['update'] })      
     name: string;
     @IsString()
     email: string;
     @IsString()
     password: string;
-    @IsNumber()
+    @IsInt()
+    @IsPositive()
     age: number;
     @IsString()
     address: string;
     @IsString()
-    @IsPositive()
     @Length(10, 15, { message: 'Phone number must be between 10 and 15 characters' })   
     phone: string;
 }
