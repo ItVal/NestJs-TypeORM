01d72fd2c08bcfce926c25a5359862d16dd64b53
Valentin Nasibu <irnasvalentin@gmail.com>
2025-05-15 22:28:08 +0200

    feat:db configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dbConfig } from 'dbConfig';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, TypeOrmModule.forRoot(dbConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
