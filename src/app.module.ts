7869bf686bcaf72dc5f2ad943f97704c89d38a79
Valentin Nasibu <irnasvalentin@gmail.com>
2025-05-16 20:41:23 +0200

    feat:create userProfile resource and create userProfile  dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dbConfig } from 'dbConfig';
+import { UserProfileModule } from './user-profile/user-profile.module';
 
 @Module({
-  imports: [UsersModule, TypeOrmModule.forRoot(dbConfig)],
+  imports: [UsersModule, TypeOrmModule.forRoot(dbConfig), UserProfileModule],
   controllers: [AppController],
   providers: [AppService],
 })
