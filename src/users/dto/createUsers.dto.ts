78dad073f13f88475aaaa45fdfe7ce87b5998694
Valentin Nasibu <irnasvalentin@gmail.com>
2025-05-16 20:31:39 +0200

    feat:create userProfile entity and define OneToOne relation

--- src/users/dto/createUsers.dto.ts
+++ src/users/dto/createUsers.dto.ts
@@ -1,19 +1,9 @@
-import { IsString, Length, IsPositive, IsInt } from 'class-validator';
+import { IsString } from 'class-validator';
 export class createUsersDto {
-    @IsString()
-    @Length(4, 20, { message: 'Name must be between 4 and 20 characters for creation', groups: ['create'] })
-    @Length(3, 20, { message: 'Name must be between 3 and 20 characters for update', groups: ['update'] })      
-    name: string;
     @IsString()
     email: string;
     @IsString()
     password: string;
-    @IsInt()
-    @IsPositive()
-    age: number;
     @IsString()
-    address: string;
-    @IsString()
-    @Length(10, 15, { message: 'Phone number must be between 10 and 15 characters' })   
-    phone: string;
+    status: string;
 }
