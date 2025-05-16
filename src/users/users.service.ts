bc41fafea6a783b4b2f66f56d38e01eea62250d2
Valentin Nasibu <irnasvalentin@gmail.com>
2025-05-16 17:36:41 +0200

    feat:update user controller

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -34,19 +34,13 @@ export class UsersService {
     }
     // update user
     async update(id: number, updateUserDto: UpdateUserDto) {
-       return await this.usersRepository.update({id}, updateUserDto);
+       return await this.usersRepository.update(id, updateUserDto);
         // const updatedUser = await this.usersRepository.findOneBy({ id });
         // if (!updatedUser) {
         //     throw new Error(`User with id ${id} not found`);
         // }
-        // return updatedUser;
+        // ret
+        // urn updatedUser;
     }
-    // async remove(id: number): Promise<void> {
-    //     const user = await this.usersRepository.findOneBy({ id });
-    //     if (!user) {
-    //         throw new Error(`User with id ${id} not found`);
-    //     }
-    //     await this.usersRepository.delete(id);
-    // }
 
 }
