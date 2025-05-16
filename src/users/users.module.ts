869cb47eb859a807a8717da16b1057ec6d8f346f
Valentin Nasibu <irnasvalentin@gmail.com>
2025-05-16 16:34:11 +0200

    feat:create user

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/entities/users.entity';
 
 @Module({
-  controllers: [UsersController]
+  imports: [TypeOrmModule.forFeature([User])], // Add your entities here
+  controllers: [UsersController],
+  providers: [UsersService]
 })
 export class UsersModule {}
