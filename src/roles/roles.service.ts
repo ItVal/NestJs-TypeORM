dc43928cfecb4d62b0d6d3ca25f503faed8562ed
Valentin Nasibu <irnasvalentin@gmail.com>
2025-05-17 20:09:07 +0200

    feat:create role service and controller

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,26 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Role } from 'src/entities/role.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class RolesService {
-  create(createRoleDto: CreateRoleDto) {
-    return 'This action adds a new role';
+  constructor(
+    @InjectRepository(Role)
+    private rolesRepository: Repository<Role>,
+  ) {}
+  //create role
+  async create(createRoleDto: CreateRoleDto) {
+    return await this.rolesRepository.save(createRoleDto);
   }
+  //findAll
+  // findAll() {
+  //   return `This action returns all roles`;
+  // }
 
-  findAll() {
-    return `This action returns all roles`;
-  }
+  // findOne(id: number) {
+  //   return `This action returns a #${id} role`;
+  // }
 
-  findOne(id: number) {
-    return `This action returns a #${id} role`;
-  }
+  // update(id: number, updateRoleDto: UpdateRoleDto) {
+  //   return `This action updates a #${id} role`;
+  // }
 
-  update(id: number, updateRoleDto: UpdateRoleDto) {
-    return `This action updates a #${id} role`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} role`;
-  }
+  // remove(id: number) {
+  //   return `This action removes a #${id} role`;
+  // }
 }
