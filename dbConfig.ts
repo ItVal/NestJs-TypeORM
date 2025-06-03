c3eb792bda21e04aa2ba4ac28666b2e1baf464c8
Valentin Nasibu <irnasvalentin@gmail.com>
2025-06-03 12:52:27 +0200

    feat:using .env

--- dbConfig.ts
+++
@@ -1,9 +0,0 @@
-import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
-
-export const dbConfig: PostgresConnectionOptions = {
-  url: 'postgresql://neondb_owner:npg_1ITvHarRfck7@ep-sparkling-breeze-a5f4yaj4-pooler.us-east-2.aws.neon.tech/cours_nestjs?sslmode=require',
-  type: 'postgres',
-  port: 3306,
-  entities: [__dirname+'/**/*.entity{.ts,.js}'],
-  synchronize: true, // FALSE in production
-};
