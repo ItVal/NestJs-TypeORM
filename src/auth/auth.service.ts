c2da07858661061f3983670b7a77198493c42cba
Valentin Nasibu <irnasvalentin@gmail.com>
2025-06-12 16:34:58 +0200

    feat:local strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { compare } from 'bcrypt';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private userService:UsersService) {}
+    async validateUser(email: string, password: string) {
+        const user = await this.userService.findByEmail(email);
+        if (!user) throw new Error('User not found');
+        const isPasswordMatch = await compare(password, user.password);
+        if (isPasswordMatch) throw new Error('Invalid password');
+        return {id: user.id}
+    }
+}
