f47049d4a163e9f8a9202ee81cbbd9810e7ceaaa
Valentin Nasibu <irnasvalentin@gmail.com>
2025-06-17 22:47:00 +0200

    feat:refresh jwt api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Post, UseGuards, Request, HttpCode, HttpStatus } from '@nes
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
 import { LocalAuthGuard } from './guards/local-auth/local-auth.guard';
+import { U } from '@faker-js/faker/dist/airline-BUL6NtOJ';
+import { RefreshJwtAuthGuard } from './guards/refresh-jwt-auth/refresh-jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -13,4 +15,16 @@ export class AuthController {
   async login(@Request() req) {
     return await this.authService.login(req.user.id);
   }
+
+
+
+
+  
+  @UseGuards(RefreshJwtAuthGuard)
+  @Post('refresh')
+  async refresh(@Request() req) {
+    return await this.authService.refreshToken(req.user.id);
+  }
+  
+
 }
