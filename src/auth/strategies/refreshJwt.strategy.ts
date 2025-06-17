6fd072d93d07f676698f66fe3b06ed07147ed554
Valentin Nasibu <irnasvalentin@gmail.com>
2025-06-17 22:33:47 +0200

    feat:refresh jwt strategy

---
+++ src/auth/strategies/refreshJwt.strategy.ts
@@ -0,0 +1,28 @@
+import { PassportStrategy } from "@nestjs/passport";
+import { Strategy, ExtractJwt } from "passport-jwt";
+import { ConfigType } from "@nestjs/config";
+import refreshJwtConfig from "src/config/refreshjwt.config";
+import { Inject, Injectable, NotFoundException } from "@nestjs/common";
+import { AuthJwtPayload } from "../types/auth-jwtPayload";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'refresh-jwt') {
+    constructor(
+        @Inject(refreshJwtConfig.KEY) 
+        private refreshJwtConfiguration: ConfigType<typeof refreshJwtConfig>) {
+
+        if (!refreshJwtConfiguration.secret) {
+             throw new NotFoundException("JWT secret is not defined in configuration");
+        }
+        super({
+              jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+              secretOrKey: refreshJwtConfiguration.secret,
+        });
+
+    }
+
+    async validate(payload: AuthJwtPayload) {
+        return {id:payload.sub};
+    }
+}
+
