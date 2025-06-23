f3b4e34e5eeb4f9cbc9f89a7ff17059bda51183b
Valentin Nasibu <irnasvalentin@gmail.com>
2025-06-23 21:22:13 +0200

    feat:enum roles

--- src/entities/users.entity.ts
+++ src/entities/users.entity.ts
@@ -3,6 +3,7 @@ import {v4 as uuidv4} from 'uuid';
 import * as bcrypt from 'bcrypt';
 import { UserProfiles } from "./userProfiles.entity";
 import { Role } from "./role.entity";
+import { Roles } from "src/auth/enums/role.enum";
 @Entity()
 export class User {
     @PrimaryGeneratedColumn("uuid")
@@ -16,6 +17,9 @@ export class User {
 
     @Column({nullable: true})
     hashedRefreshToken: string;
+
+    @Column({ type: 'enum', enum: Roles, default: Roles.USER})
+    roles:Role;
 
     @Column()
     status: string;
