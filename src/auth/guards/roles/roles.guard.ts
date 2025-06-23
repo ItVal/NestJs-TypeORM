288d399c8cceea13db067247563db4decd59ab98
Valentin Nasibu <irnasvalentin@gmail.com>
2025-06-23 22:02:41 +0200

    feat:roles guard

---
+++ src/auth/guards/roles/roles.guard.ts
@@ -0,0 +1,19 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { ROLES_KEY } from 'src/auth/decorators/role.decorator';
+import { Roles } from 'src/auth/enums/role.enum';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+  canActivate(context: ExecutionContext): boolean  {
+    const requiredRoles = this.reflector.getAllAndOverride<Roles[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    const user = context.switchToHttp().getRequest().user;
+    const hasRequiredRole = requiredRoles.some(role => user.role = role);
+    return hasRequiredRole
+  }
+}
