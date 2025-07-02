cb64f7f184045ff124a4afe8fe4b36c16f23ee15
Valentin Nasibu <irnasvalentin@gmail.com>
2025-07-02 19:49:23 +0200

    fix:googgle strategy

--- src/auth/strategies/google.stratgy.ts
+++ src/auth/strategies/google.stratgy.ts
@@ -1,33 +1,36 @@
-import { Inject, Injectable } from "@nestjs/common";
-import { ConfigType } from "@nestjs/config";
-import { PassportStrategy } from "@nestjs/passport";
-import { Strategy, VerifyCallback } from "passport-google-oauth20";
-import googleOauthConfig from "src/config/google.oauth.config";
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { Injectable } from '@nestjs/common';
+import { Request } from 'express';
+import googleConfig from 'src/config/google.oauth.config';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
-  constructor(
-    @Inject(googleOauthConfig.KEY) private googleConfiguration: googleOauthConfig,
-  ) {
+  constructor() {
+    const config = googleConfig();
+
     super({
-      clientID: googleConfiguration.clientID,
-      clientSecret: googleConfiguration.clientSecret,
-      callbackURL: googleConfiguration.callbackURL,
+      clientID: config.clientID ?? '',
+      clientSecret: config.clientSecret ?? '',
+      callbackURL: config.callbackUrl,
       scope: ['email', 'profile'],
-      passReqToCallback: true, // Vérifiez si cette option est supportée
+      passReqToCallback: true, // Permet de passer req en premier paramètre
     });
   }
 
+  // ✅ avec req en premier paramètre
   async validate(
+    req: Request,
     accessToken: string,
     refreshToken: string,
     profile: any,
     done: VerifyCallback,
   ): Promise<any> {
+    const { name, emails, photos } = profile;
     const user = {
-      email: profile.emails[0].value,
-      firstName: profile.name.givenName,
-      lastName: profile.name.familyName,
+      email: emails?.[0]?.value,
+      name: `${name?.givenName ?? ''} ${name?.familyName ?? ''}`.trim(),
+      picture: photos?.[0]?.value,
       accessToken,
     };
     done(null, user);
--- src/config/google.oauth.config.ts
+++ src/config/google.oauth.config.ts
@@ -5,3 +5,9 @@ export default registerAs('googleOAuth', () => ({
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackUrl: process.env.GOOGLE_CALLBACK_URL
 }));
+export const KEY = 'GoogleOauthConfig';
+interface GoogleOAuthConfig {
+    clientID: string;
+    clientSecret: string;
+    callbackUrl: string;
+};
